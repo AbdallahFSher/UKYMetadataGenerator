135f4326482ba510deebc22197972646a69c47ef
Calvin Higdon <calvindhigdon@Gmail.com>
2025-01-28 23:05:25 -0500

    Added JSON Button and Label

--- app/source/main.py
+++ app/source/main.py
@@ -4,14 +4,50 @@
 # Description: Entry point for application
 # Last Modified: Jan. 27, 2025
 
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import QtWidgets
+import json
 import sys
 
-# sys.argv contains the list of command-line arguments passed to a Python script
-# could be used to create command-line interface for the application, enabling scripting
-app = QApplication(sys.argv)
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.button = QtWidgets.QPushButton('Select a JSON file...')
+        self.button.clicked.connect(self.LoadJsonButtonClicked)
+        
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.button)
 
-window = QWidget()
-window.show()
+        self.jsonText = QtWidgets.QLabel()
+        self.jsonText.setWordWrap(True)
+        self.jsonText.setMinimumSize(400, 200)
 
-app.exec()
+        self.jsonScrollArea = QtWidgets.QScrollArea()
+        self.jsonScrollArea.setWidgetResizable(True)
+        self.jsonScrollArea.setWidget(self.jsonText)
+        self.jsonScrollArea.setLayout(QtWidgets.QVBoxLayout())
+
+        layout.addWidget(self.jsonScrollArea)
+
+    def LoadJsonButtonClicked(self):
+        print('Button Clicked')
+        # options = QtWidgets.QFileDialog.options()
+        # options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        file_name, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open JSON File', '', 'JSON Files (*.json)')
+        if file_name:
+            print(file_name)
+            with open(file_name, 'r') as file:
+                data = json.load(file)
+                print(data)
+            self.jsonText.setText(json.dumps(data, indent=4))
+            return data
+        return None
+        
+if __name__ == '__main__':
+    # sys.argv contains the list of command-line arguments passed to a Python script
+    # could be used to create command-line interface for the application, enabling scripting
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = Window()
+    window.resize(800, 600)
+    window.show()
+    sys.exit(app.exec())
