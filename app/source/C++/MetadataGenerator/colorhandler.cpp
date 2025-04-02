93553cebcc61842fc421515f2e89f720c727ea30
Calvin Higdon <calvindhigdon@Gmail.com>
2025-04-02 15:41:30 -0400

    Color Scheme Changes

--- app/source/C++/MetadataGenerator/colorhandler.cpp
+++ app/source/C++/MetadataGenerator/colorhandler.cpp
@@ -3,18 +3,19 @@
 
 #include <QColor>;
 #include <QPalette>;
+#include "node.h"
 
 using namespace std;
 
 ColorHandler::ColorHandler() {
-        //0. orange
-        //1. blue
-        //2. light-blue
-        //3. pink
-        //4. red
-        //5. blue-green
-        //6. gray
-    this->palette.setColor(QPalette::Window, this->colorList[6]);
+        //0. light-red
+        //1. yellow
+        //2. green
+        //3. light-blue
+        //4. blue-green
+        //5. rose
+        //6. pink
+    this->palette.setColor(QPalette::Window, this->bg);
 }
 
 QPalette ColorHandler::getPalette() {
@@ -24,4 +25,13 @@ QPalette ColorHandler::getPalette() {
 
 QColor* ColorHandler::getColors() {
     return this->colorList;
+}
+
+void ColorHandler::setColors(std::vector<Node*> nodes) {
+    for (Node *node : nodes) {
+        QColor color = colorList[node->nodeVariant];
+        QString style = "background: rgb(%1, %2, %3);";
+        node->setStyleSheet(style.arg(color.red()).arg(color.green()).arg(color.blue()) + "; color:black");
+    }
+
 }
--- app/source/C++/MetadataGenerator/mainwindow.cpp
+++ app/source/C++/MetadataGenerator/mainwindow.cpp
@@ -9,11 +9,13 @@ using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
+    std::vector<Node*> nodes = vector<Node*>();
     ui->setupUi(this);
-    //QLayout *layout = this->ui->nodeHolderr;
 
-    Node *beans = new Node(this->ui->nodeHolder, "bomb");
-    Node *beans2 = new Node(this->ui->nodeHolder, "bones");
+    Node *beans = new Node(this->ui->nodeHolder, 0);
+    Node *beans2 = new Node(this->ui->nodeHolder, 1);
+    nodes.push_back(beans);
+    nodes.push_back(beans2);
     beans->setText("One");
     beans->adjustSize();
     beans2->setText("Two");
@@ -22,6 +24,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     ColorHandler *colorHandler = new ColorHandler();
 
     this->setPalette(colorHandler->getPalette());
+    colorHandler->setColors(nodes);
 
     //layout->addWidget(beans);
     //layout->addWidget(beans2);
